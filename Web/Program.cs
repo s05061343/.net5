ce3ba89bef104fb8adba58bb05fd2124286830e0
s05061343 <s05061343@gmail.com>
2021-10-04 22:14:16 +0800

    WIP

--- Web/Program.cs
+++ Web/Program.cs
@@ -25,45 +25,5 @@ namespace WebUI
                 {
                     webBuilder.UseStartup<Startup>();
                 });
-
-        private static async Task RunProgram()
-        {
-            try
-            {
-                // 建立 scheduler
-                StdSchedulerFactory factory = new StdSchedulerFactory();
-                IScheduler scheduler = await factory.GetScheduler();
-
-                // 建立 Job
-                IJobDetail job = JobBuilder.Create<ShowDataTimeJob>()
-                    .WithIdentity("job1", "group1")
-                    .Build();
-
-                // 建立 Trigger，每秒跑一次
-                ITrigger trigger = TriggerBuilder.Create()
-                    .WithIdentity("trigger1", "group1")
-                    .StartNow()
-                    .WithSimpleSchedule(x => x
-                        .WithIntervalInSeconds(1)
-                        .RepeatForever())
-                    .Build();
-
-                // 加入 ScheduleJob 中
-                await scheduler.ScheduleJob(job, trigger);
-
-                // 啟動
-                await scheduler.Start();
-
-                // 執行 10 秒
-                await Task.Delay(TimeSpan.FromSeconds(10));
-
-                // say goodbye
-                await scheduler.Shutdown();
-            }
-            catch (SchedulerException se)
-            {
-                await Console.Error.WriteLineAsync(se.ToString());
-            }
-        }
     }
 }
